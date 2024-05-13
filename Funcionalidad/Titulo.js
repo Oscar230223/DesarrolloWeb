4f72a778b520c0f314b81df839aa50e199e7c6ce
Janeth <lunajanethc45@gmail.com>
2024-05-13 01:38:26 -0500

    terminado srs

--- Funcionalidad/Titulo.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const dynamicTitle = document.getElementById("dynamic-title");
-    const titleText = "Título Dinámico"; // Puedes cambiar esto por el título que desees
-    dynamicTitle.textContent = titleText;
-});
